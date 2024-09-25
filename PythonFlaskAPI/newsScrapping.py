4c7767bfeba285c3510f879f00667ee8564acc36
Khushboo Goel <khushboo.goel@cginfinity.com>
2024-09-25 13:54:52 +0530

    openai code added

--- PythonFlaskAPI/newsScrapping.py
+++ PythonFlaskAPI/newsScrapping.py
@@ -4,6 +4,7 @@ from urllib import response
 import requests
 import pandas as pd
 import http.client
+import openai
 
 from flask import jsonify,request
 from PythonFlaskAPI import app
@@ -41,3 +42,31 @@ def filterNews():
     data = res.read()
     jsondata = json.loads(data.decode("utf-8"))
     return jsondata
+
+
+@app.route('/openAIIntegration', methods=['GET'])
+def openAIIntegration(): 
+    openai.api_key = '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'
+    question = "cricket news"
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          {
+            "role": "system",
+            "content": "search news"
+          },
+          {
+            "role": "user",
+            "content": question
+          }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+
+    processed = response["choices"][0]["message"]["content"]
+    return processed
+    #response.choices[0].message.content
